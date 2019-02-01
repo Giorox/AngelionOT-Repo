e17ab5acb09e610feee425a95f93038423e979d6
Giorox <31557160+Giorox@users.noreply.github.com>
2019-02-01 16:50:30 -0200

    Some Fixes
    -Fixed Minotaur Hell Quest (#199)
    -Fixed Cocoons bug (#226)
    -Fixed some Rook spawns

--- data/actions/scripts/quests/TerraAmuletCocoons.lua
+++ data/actions/scripts/quests/TerraAmuletCocoons.lua
@@ -1,7 +1,8 @@
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(item.itemid == 7585) then
+	if(item.itemid == 7585 and item.uniqueid ~= nil) then
 		doTransformItem(item.uid, item.itemid + 1)
 		value = getPlayerStorageValue(cid, 23000)
+		
 		if((value > 0) and (value < 5)) then
 			setPlayerStorageValue(cid, 23000, value+1)
 		end
@@ -13,8 +14,28 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 		local cocoon3 = getThing(23003)
 		local cocoon4 = getThing(23004)
 		
-		if((cocoon.itemid == 7586) and (cocoon.itemid == 7586) and (cocoon3.itemid == 7586) and (cocoon4.itemid == 7586)) then
+		if((cocoon1.itemid == 7586) and (cocoon2.itemid == 7586) and (cocoon3.itemid == 7586) and (cocoon4.itemid == 7586)) then
 			doSummonCreature(monster, pos)
+		end
+	elseif (item.itemid == 7585) then
+		local random = math.random(1,100)
+		if(random < 40) then
+			doTransformItem(item.uid, item.itemid + 1)
+			doSendMagicEffect(toPosition, 16)
+			doCreatureSay(cid, "Ploft.", TALKTYPE_ORANGE_1, nil, nil, fromPosition)
+			doSummonCreature("Spider", toPosition)
+			return true
+		elseif(random < 80) then
+			doTransformItem(item.uid, item.itemid + 1)
+			doSendMagicEffect(toPosition, 16)
+			doCreatureSay(cid, "Ploft.", TALKTYPE_ORANGE_1, nil, nil, fromPosition)
+			doSummonCreature("Poison Spider", toPosition)
+			return true
+		elseif(random < 100) then
+			doTransformItem(item.uid, item.itemid + 1)
+			doSendMagicEffect(toPosition, 16)
+			doCreatureSay(cid, "Ploft.", TALKTYPE_ORANGE_1, nil, nil, fromPosition)
+			return true
 		end
 	end
 end
