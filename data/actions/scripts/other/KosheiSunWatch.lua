fd418e7e4533f00e9ff655bb809265c02ffc9e70
Giovanni Rebouças <giovannireboucas@hotmail.com>
2019-06-23 17:06:05 -0300

    Fixes and Reviews
    -Fixed map void showing in some positions (#307)
    -Fixed Koshei's reset (#306)
    -Fixed Jack Fate not sending players to Goroma (#305)
    -Finished Mustelids Category (#279)(#227)

--- data/actions/scripts/other/KosheiSunWatch.lua
+++ data/actions/scripts/other/KosheiSunWatch.lua
@@ -5,11 +5,12 @@ function onUse(cid, item, fromPosition, itemEx, toPosition)
 	local statue4 = getThing(4003)
 	local statue5 = getThing(4004)
 	if((statue1.itemid == 3697) and (statue2.itemid == 3698) and (statue3.itemid == 3698) and (statue4.itemid == 3697) and (statue5.itemid == 3697)) then
-		doTransformItem(4006, 7525)
+		coffinladder = getThingFromPos({x=33273,y=32458,z=8, stackpos=1}).uid
+		doTransformItem(coffinladder, 7525)
 		doTransformItem(4007, 7521)
 		doCreatureSay(cid, "CLICK", TALKTYPE_ORANGE_1, nil, nil, {x=33275,y=32451,z=8})
 	else
-		doCreatureSay(cid, "CRANK", TALKTYPE_ORANGE_1)
+		doCreatureSay(cid, "CRANK", TALKTYPE_ORANGE_1, nil, nil, {x=33281,y=32447,z=8})
 	end
 	
 end
